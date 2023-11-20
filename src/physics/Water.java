a3f92438d266d30475f58460683c336ebe60e1b2
Rafael Dias <fafague.dias@gmail.com>
2023-11-20 16:29:41 +0000

    Update Water.java

--- src/physics/Water.java
+++ src/physics/Water.java
@@ -1,5 +1,8 @@
 package physics;
 
+import processing.core.PApplet;
+import tools.SubPlot;
+
 public class Water extends Fluid {
 
     private float waterHeight;
@@ -9,5 +12,18 @@ public class Water extends Fluid {
         super(1000.0f);
         this.waterHeight = waterHeight;
         this.color = color;
+    }
+
+    public boolean isInside(Mover m) {
+        return (m.getPos().y <= waterHeight);
+    }
+
+    public void display(PApplet p, SubPlot plt) {
+        p.pushStyle();
+        p.noStroke();
+        p.fill(color);
+        float[] pp = plt.getBox(0, 0, plt.getWindow()[1], waterHeight);
+        p.rect(pp[0], pp[1], pp[2], pp[3]);
+        p.popStyle();
     }
 }
