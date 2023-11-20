bd7ee51289fdf2278e9151b55ed0a1be9ca6ed48
Rafael Dias <fafague.dias@gmail.com>
2023-11-20 16:39:40 +0000

    Create FallingBodyApp.java

---
+++ src/physics/FallingBodyApp.java
@@ -0,0 +1,52 @@
+package physics;
+
+import processing.core.PApplet;
+import setup.IProcessingApp;
+import tools.SubPlot;
+
+public class FallingBodyApp implements IProcessingApp {
+
+    public static final float dimX = 20;
+    public static final float dimY = 15;
+    private static final float g = -9.8f;
+    private static final float mass = 80;
+    private static final float radius = 0.5f;
+    private final double window[] = {0, dimX, 0, dimY};
+    private float viewport[] = {0f, 0f, 1f, 1f};
+    private SubPlot plt;
+    // private Body ball;
+    private Water water;
+    private Air air;
+    private float speedUp = 0.5f;
+    private float timer;
+
+    @Override
+    public void setup(PApplet parent) {
+        plt = new SubPlot(window, viewport, parent.width, parent.height);
+        // ball = new Body(new PVector(0, 12), new PVector(8, 6),
+        //            mass, radius, parent.color(255, 255, 0));
+        water = new Water(4, parent.color(0, 255, 255));
+        air = new Air();
+        timer = 0.0f;
+    }
+
+    @Override
+    public void draw(PApplet parent, float dt) {
+
+    }
+
+    @Override
+    public void keyPressed(PApplet parent) {
+
+    }
+
+    @Override
+    public void mousePressed(PApplet parent) {
+
+    }
+
+    @Override
+    public void mouseDragged(PApplet parent) {
+
+    }
+}
