da3570b3cf48943e7e4b94cdac5aeb4e16c0dcdc
Rafael Dias <fafague.dias@gmail.com>
2023-11-22 18:51:03 +0000

    changes

--- src/physics/ParticleSystem.java
+++ src/physics/ParticleSystem.java
@@ -10,22 +10,23 @@ import java.util.List;
 public class ParticleSystem extends Body{
 
     private List<Particle> particles;
-    private float lifetime;
     private PSControl psc;
 
     protected ParticleSystem(PVector pos, PVector vel, float mass,
-                             float radius, int color, float lifetime,
-                             PSControl psc) {
-        super(pos, vel, mass, radius, color);
-        this.lifetime = lifetime;
+                             float radius, PSControl psc) {
+        super(pos, vel, mass, radius, 0);
         this.psc = psc;
         this.particles = new ArrayList<Particle>();
+    }
+
+    public PSControl getPSControl() {
+        return psc;
     }
 
     @Override
     public void move(float dt) {
         super.move(dt);
-        addParticle();
+        addParticles(dt);
         for(int i = particles.size()-1; i>=0; i--) {
             Particle p = particles.get(i);
             p.move(dt);
@@ -35,8 +36,20 @@ public class ParticleSystem extends Body{
         }
     }
 
-    private void addParticle() {
-        Particle particle = new Particle(pos, psc.getRndVel(), radius, color, lifetime);
+    public void addParticles(float dt) {
+        float particlesPerFrame = psc.getFlow() * dt;
+        int n = (int) particlesPerFrame;
+        float f = particlesPerFrame - n;
+        for (int i=0; i<n; i++) {
+            addOneParticle();
+        }
+        if (Math.random() < f) {
+            addOneParticle();
+        }
+    }
+
+    private void addOneParticle() {
+        Particle particle = new Particle(pos, psc.getRndVel(), psc.getRndRadius(), psc.getColor(), psc.getRndLifetime());
         particles.add(particle);
     }
 
--- src/physics/ParticleSystemApp.java
+++ src/physics/ParticleSystemApp.java
@@ -14,7 +14,10 @@ public class ParticleSystemApp implements IProcessingApp {
     private double[] window = {-10, 10, -10, 10};
     private float[] viewport = {0, 0, 1, 1};
     private SubPlot plt;
-    private float[] velControl = {PApplet.radians(90), PApplet.radians(20), 1, 3};
+    private float[] velParams = {PApplet.radians(90), PApplet.radians(20), 1, 3};
+    private float[] lifetimeParams = {3, 5};
+    private float[] radiusParams = {0.1f, 0.5f};
+    private float flow = 50;
 
     @Override
     public void setup(PApplet parent) {
@@ -35,9 +38,12 @@ public class ParticleSystemApp implements IProcessingApp {
             ps.display(parent, plt);
         }
 
-        velControl[0] = PApplet.map(parent.mouseX, 0, parent.width,
+        velParams[0] = PApplet.map(parent.mouseX, 0, parent.width,
                 PApplet.radians(0), PApplet.radians(360));
-
+        for (ParticleSystem ps : pss) {
+            PSControl psc = ps.getPSControl();
+            psc.setVelParams(velParams);
+        }
     }
 
     @Override
@@ -52,7 +58,7 @@ public class ParticleSystemApp implements IProcessingApp {
         int cor = parent.color(parent.random(255), parent.random(255), parent.random(255));
         float lifespan = parent.random(1, 3);
 
-        PSControl psc = new PSControl(velControl);
+        PSControl psc = new PSControl(velParams);
         ParticleSystem ps = new ParticleSystem(new PVector((float)ww[0], (float)ww[1]), new PVector() , 1f, 0.2f,
                 cor, lifespan, psc);
         pss.add(ps);
